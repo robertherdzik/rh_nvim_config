393c318c3b8241871c0bdbbd715ab7db0fb2afa5
robertherdzik <robert.herdzik@yahoo.com>
2024-06-11 15:35:36 +0200

    Add new remaps

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -1,7 +1,22 @@
+function FindTODOGlobally()
+	require('telescope.builtin').live_grep({ default_text = "// ↱TODO➤" })
+end
+
+-- Higlight search for entire regex e.g if we do /vim.* - it will higlight satyfying part of the lane
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Never has less than x lines below once scrolling
+vim.opt.scrolloff = 8
+
+-- Puts paste action into void register then we can hit `p` and dont lose what we had in register
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
 return {
 	vim.keymap.set("n", "<leader>pv", vim.cmd.Ex),
 	-- Define the keymap in normal mode Tuist 
 	vim.api.nvim_set_keymap('n', '<leader>xb', ':!tuist build<CR>', { noremap = true, silent = true }),
-	vim.api.nvim_set_keymap('n', '<leader>xt', ':!tuist test<CR>', { noremap = true, silent = true })
-
+	vim.api.nvim_set_keymap('n', '<leader>xt', ':!tuist test<CR>', { noremap = true, silent = true }),
+	vim.api.nvim_set_keymap('n', '<leader>st', ':lua FindTODOGlobally()<CR>', { noremap = true, silent = true }),
 }
+
