8462d9047deb3ac596edd44c6b326c5174b483b1
robertherdzik <robert.herdzik@yahoo.com>
2024-06-11 13:04:54 +0200

    Add telescope config

---
+++ lua/custom/plugins/telescope.lua
@@ -0,0 +1,16 @@
+local telescope = require('telescope')
+
+telescope.setup {
+defaults = { 
+
+    path_display = function (_, path)
+      -- Show only last files not entire path 
+      local tail = require('telescope.utils').path_tail(path)
+      return tail
+    end,
+  },
+}
+
+pcall(telescope.load_extension, "fzf")
+
+return {}
