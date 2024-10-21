656df6dd0e87b8e4d9d6ea993d439959ab92c324
robertherdzik <robert.herdzik@yahoo.com>
2024-10-21 21:11:03 +0100

    Add theme color

---
+++ lua/custom/plugins/barbecue.lua
@@ -0,0 +1,13 @@
+-- Plugin show path at the top of the file
+return {
+  'utilyre/barbecue.nvim',
+  name = 'barbecue',
+  version = '*',
+  dependencies = {
+    'SmiteshP/nvim-navic',
+    'nvim-tree/nvim-web-de:vicons', -- optional dependency
+  },
+  opts = {
+    -- configurations go here
+  },
+}
