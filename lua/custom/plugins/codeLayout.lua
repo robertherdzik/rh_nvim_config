e6a1810eeae189c74ee1adf42905bcb3c40a6cf1
robertherdzik <robert.herdzik@yahoo.com>
2024-10-26 23:03:45 +0100

    Add swift autoclosing real

--- lua/custom/plugins/codeLayout.lua
+++ lua/custom/plugins/codeLayout.lua
@@ -3,4 +3,11 @@ vim.opt.shiftwidth = 3 -- Number of spaces for indentation
 vim.opt.softtabstop = 3 -- Insert spaces for the tab key
 vim.opt.expandtab = true -- Use spaces instead of tabs
 
+-- for .siwft files add closing method bracket
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'swift',
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, 'i', '{', '{<CR>}<Esc>O', { noremap = true, silent = true })
+  end,
+})
 return {}
