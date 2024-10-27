56dcb28e0dbb8fc22885309322219e8ab9e54a67
robertherdzik <robert.herdzik@yahoo.com>
2024-10-27 15:58:41 +0000

    Make floating term wider

--- lua/custom/plugins/floatingTerminal.lua
+++ lua/custom/plugins/floatingTerminal.lua
@@ -1,8 +1,8 @@
 -- floatign terminal
 
 -- Set the size of the Floaterm window
-vim.g.floaterm_width = 0.9 -- 90% of the screen width
-vim.g.floaterm_height = 0.9 -- 90% of the screen height
+vim.g.floaterm_width = 0.95
+vim.g.floaterm_height = 0.95
 vim.api.nvim_set_keymap('n', '<leader>t', ':FloatermNew<CR>', { noremap = true, silent = true })
 
 return {
