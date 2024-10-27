379138211c38d3f3277bac6b5adb1c5eec294de5
robertherdzik <robert.herdzik@yahoo.com>
2024-10-27 16:11:02 +0000

    Add new colors pallete to floating term

--- lua/custom/plugins/floatingTerminal.lua
+++ lua/custom/plugins/floatingTerminal.lua
@@ -5,6 +5,29 @@ vim.g.floaterm_width = 0.95
 vim.g.floaterm_height = 0.95
 vim.api.nvim_set_keymap('n', '<leader>t', ':FloatermNew<CR>', { noremap = true, silent = true })
 
+-- Configure floating terminal colors (using highlight commands)
+vim.cmd [[
+ " Dracula theme colors
+  "highlight Floaterm guibg=#282a36 guifg=#f8f8f2
+  "highlight FloatermBorder guibg=#282a36 guifg=#6272a4
+
+  " Gruvbox theme colors
+  "highlight Floaterm guibg=#282828 guifg=#ebdbb2
+  "highlight FloatermBorder guibg=#282828 guifg=#928374
+
+  " Nord theme colors
+  "highlight Floaterm guibg=#2E3440 guifg=#D8DEE9
+  "highlight FloatermBorder guibg=#2E3440 guifg=#81A1C1
+
+  " One Dark theme colors
+  highlight Floaterm guibg=#282c34 guifg=#abb2bf
+  highlight FloatermBorder guibg=#282c34 guifg=#61afef
+
+  " Tokyo Night theme colors
+  "highlight Floaterm guibg=#1f2335 guifg=#c0caf5
+  "highlight FloatermBorder guibg=#1f2335 guifg=#565f89
+]]
+
 return {
   'voldikss/vim-floaterm',
 }
