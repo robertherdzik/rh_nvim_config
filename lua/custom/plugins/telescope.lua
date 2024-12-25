24797bf61b4f6b60bfbad05dedc823261f84ef0b
robertherdzik <robert.herdzik@yahoo.com>
2024-12-25 22:56:11 +0100

    Adjust telescope path displaying

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,15 +1,22 @@
-local telescope = require('telescope')
+local telescope = require 'telescope'
 
 telescope.setup {
-defaults = { 
-    path_display = function (_, path)
-      -- Show only last files not entire path 
-      local tail = require('telescope.utils').path_tail(path)
-      return tail
+  defaults = {
+    path_display = function(_, path)
+      local utils = require 'telescope.utils'
+      local sep = utils.get_separator()
+      local split_path = vim.split(path, sep, { plain = true })
+
+      if #split_path < 3 then
+        return path -- Return the full path if it has fewer than three segments
+      end
+
+      -- Show the last two folders and the file
+      return table.concat(split_path, sep, #split_path - 2)
     end,
   },
 }
 
-pcall(telescope.load_extension, "fzf")
+pcall(telescope.load_extension, 'fzf')
 
 return {}
