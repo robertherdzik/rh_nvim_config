207797510c8079761fdbb79c10af2773230ac8bc
robertherdzik <robert.herdzik@yahoo.com>
2024-12-28 10:53:55 +0100

    Fix issue with ont showing record mode on macros

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,10 +3,14 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add any options here
+    routes = {
+      {
+        view = 'notify',
+        filter = { event = 'msg_showmode' },
+      },
+    },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
